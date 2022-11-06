cda16dee2e17fd3bb942a72fc69c8cd2a06044c9
HαckͥDeͣeͫm <92859246+dimasdeem@users.noreply.github.com>
2022-11-06 21:00:57 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -9,19 +9,19 @@ const catalog = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
 "product": {
 "productImage": messa.imageMessage, 
 "productId": "5489299044451325",
-"title": `Jasa Sewa Yushino Md`,
+"title": `Donasi Seikhlas nya`,
 "description": `HALO BANG`,
 "currencyCode": "IDR",
 "bodyText": wm,
 "footerText": wm,
-"priceAmount1000": "3000",
+"priceAmount1000": "30000",
 "productImageCount": 1,
 "firstImageId": 1,
 "salePriceAmount1000": "10000000",
 "retailerId": wm,
-"url": "wa.me/6281361281833"
+"url": "wa.me/6285156581890"
 },
-"businessOwnerJid": "6281361281833@s.whatsapp.net",
+"businessOwnerJid": "6285156581890@s.whatsapp.net",
 }
 }), { userJid: m.chat, quoted: ftroli })    
 
