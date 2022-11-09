bc5e0eec1bf343b7a5a51d7ad0be3f4f1878375d
HαckͥDeͣeͫm <92859246+dimasdeem@users.noreply.github.com>
2022-11-09 13:32:42 +0700

    Update _antitag.js

--- plugins/_antitag.js
+++ plugins/_antitag.js
@@ -1,7 +1,7 @@
 let handler = m => m
 
 handler.before = async function (m) {
-   if (m.mentionedJid.length >= 20) await conn.send2ButtonDoc(m.chat, `Ada Apa Tag Saya Kak`, wm, 'Menu', '.menu', 'SewaBot', '.sewabot', m, { contextInfo: { forwardingScore: fsizedoc, externalAdReply: { body: 'Tes', containsAutoReply: true, mediaType: 1, mediaUrl: hwaifu.getRandom(),  renderLargerThumbnail: true, showAdAttribution: true, sourceId: 'Tes', sourceType: 'PDF', previewType: 'PDF', sourceUrl: sgc, thumbnail: fs.readFileSync('./thumbnail.jpg'), thumbnailUrl: sgc, title: 'ɪ ɴ ᴛ ʀ ᴏ ᴄ ᴀ ᴅ ᴜ ᴛ ɪ ᴏ ɴ'}}})
+   if (m.mentionedJid.length >= 20) await conn.send2ButtonDoc(m.chat, `Ada Apa Tag Saya Kak`, wm, 'Menu', '.menu', 'Donasi', '.donasi', m, { contextInfo: { forwardingScore: fsizedoc, externalAdReply: { body: 'Hⱥck𝓓eeᴍ-Mdོ', containsAutoReply: true, mediaType: 1, mediaUrl: hwaifu.getRandom(),  renderLargerThumbnail: true, showAdAttribution: true, sourceId: 'Tes', sourceType: 'PDF', previewType: 'PDF', sourceUrl: sgc, thumbnail: fs.readFileSync('./thumbnail.jpg'), thumbnailUrl: sgc, title: 'ɪ ɴ ᴛ ʀ ᴏ ᴄ ᴀ ᴅ ᴜ ᴛ ɪ ᴏ ɴ'}}})
     }
 
 export default handler
