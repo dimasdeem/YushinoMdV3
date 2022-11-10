60e83454e352082f5f25da08c72103ec67b96b37
HαckͥDeͣeͫm <92859246+dimasdeem@users.noreply.github.com>
2022-11-10 19:12:05 +0700

    Update internet-memeindo.js

--- plugins/internet-memeindo.js
+++ plugins/internet-memeindo.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text }) => {
 let res = await fetch('https://raw.githubusercontent.com/HasamiAini/wabot_takagisan/main/whatsapp%20bot%20takagisan/whatsapp%20bot%20takagisan/lib/memeindo.json')
 let json = await res.json();
 let url = json[Math.floor(Math.random() * json.length)]
-await conn.sendButtonImg(m.chat, await (await fetch(url.image)).buffer(), 'memeindo', 'Next', '.meme', m)
+await conn.sendButtonImg(m.chat, await (await fetch(url.image)).buffer(), 'memeindo', 'Next', 'Next', '.meme', m)
 }
 handler.command = /^(meme)$/i
 handler.tags = ['internet']
