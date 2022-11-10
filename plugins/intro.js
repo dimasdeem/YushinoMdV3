912966c7ccfbed4262789d21a22c683161649b39
HαckͥDeͣeͫm <92859246+dimasdeem@users.noreply.github.com>
2022-11-10 19:05:09 +0700

    Update intro.js

--- plugins/intro.js
+++ plugins/intro.js
@@ -14,9 +14,9 @@ let krtu = `☰⃟⟣⟔ Yᴏᴜʀ Cᴀʀᴅ Iɴᴛʀᴏ ⟓⟢⃟
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, krtu, wm,'MENU','.menu', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://chat.whatsapp.com/",
+    mediaUrl: "https://www.instagram.com/hck.deem",
     mediaType: "VIDEO",
-    description: "https://www.instagram.com/", 
+    description: "ᴹᴿ᭄ HαckͥDeͣeͫm ×፝֟͜×", 
     title: 'Iɴᴛʀᴏᴄᴀᴅᴜᴛɪᴏɴ',
     body: wm,
     thumbnail: thumb,
