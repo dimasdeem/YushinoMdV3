1582c276beb2b44e2ad3d7de7ed1c393dd60b369
HαckͥDeͣeͫm <92859246+dimasdeem@users.noreply.github.com>
2022-11-11 21:35:26 +0700

    Update downloader-tiktokmp3.js

--- plugins/downloader-tiktokmp3.js
+++ plugins/downloader-tiktokmp3.js
@@ -2,14 +2,14 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `contoh:\n ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
-    let res = (await axios.get(API('ana', 'api/download/tiktok2', { url: args[0], }, ))).data;
+    let res = (await axios.get(API('ana', 'api/download/tiktok2', { url: args[0], }, 'apikey' ))).data;
     if (res.status != 200) throw res.message;
     if (!res) throw res.message;
     
 conn.sendFile(m.chat, res.audio, 'error.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: false, seconds: 0,contextInfo: {
-         externalAdReply: { showAdAttribution: true,
+         externalAdReply: { showAdAttribution: true,
  mediaUrl: snh,
     mediaType: 2, 
     description: snh,
